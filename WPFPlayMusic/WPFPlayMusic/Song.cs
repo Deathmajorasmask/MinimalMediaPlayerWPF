5380d7f7d652133a27915ea08131a520e90bc175
Deathmajorasmask <zijaham_link@hotmail.com>
2023-06-12 01:15:22 -0600

    First Commit / Create Dev Logic

---
+++ WPFPlayMusic/WPFPlayMusic/Song.cs
@@ -0,0 +1,22 @@
+﻿namespace WPFPlayMusic
+{
+    internal class Song
+    {
+
+		public int IDMusic
+		{ get; set; }
+
+		public string SName
+		{ get; set; }
+
+		public string SArtist
+		{ get; set; }
+
+		public string SDuracion
+		{ get; set; }
+
+		public string SGenero
+		{ get; set; }
+
+	}
+}
